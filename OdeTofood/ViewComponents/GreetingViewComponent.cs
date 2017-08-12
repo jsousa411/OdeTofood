5d4f17d95879d84362ee7ee75da5708bab330e1e
Joao Sousa <jsousa411@gmail.com>
2017-08-12 12:35:03 -0700

    Layout views and partial views setup

---
+++ OdeTofood/ViewComponents/GreetingViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeTofood.Services;
+using System.Threading.Tasks;
+
+namespace OdeTofood.ViewComponents
+{
+    public class GreetingViewComponent : ViewComponent
+    {
+        private IGreeter _greeter;
+
+        public GreetingViewComponent(IGreeter greeter)
+        {
+
+            _greeter = greeter;
+
+        }
+        public Task<IViewComponentResult> InvokeAsync()
+        {
+
+            var model = _greeter.GetGreeting();
+            return Task.FromResult<IViewComponentResult>(View("Default", model));
+            
+        }
+
+        
+    }
+}
